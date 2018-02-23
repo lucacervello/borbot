32b4f8b905479cce6e2c3e1e0eb6985e0af64d0d
lucacervello <luca.cervello@gmail.com>
2018-02-23 17:28:01 +0100

    initial commit

---
+++ src/borbot/core.clj
@@ -0,0 +1,39 @@
+(ns borbot.core
+  (:require [clojure.core.async :refer [<!!]]
+            [clojure.string :as str]
+            [environ.core :refer [env]]
+            [morse.handlers :as h]
+            [morse.polling :as p]
+            [morse.api :as t])
+  (:gen-class))
+
+; TODO: fill correct token
+(def token (env :telegram-token))
+
+
+(h/defhandler handler
+
+  (h/command-fn "start"
+    (fn [{{id :id :as chat} :chat}]
+      (println "Bot joined new chat: " chat)
+      (t/send-text token id "Welcome to borbot!")))
+
+  (h/command-fn "help"
+    (fn [{{id :id :as chat} :chat}]
+      (println "Help was requested in " chat)
+      (t/send-text token id "Help is on the way")))
+
+  (h/message-fn
+    (fn [{{id :id} :chat :as message}]
+      (println "Intercepted message: " message)
+      (t/send-text token id (apply str (repeat (inc (rand-int 10)) "bor"))))))
+
+
+(defn -main
+  [& args]
+  (when (str/blank? token)
+    (println "Please provde token in TELEGRAM_TOKEN environment variable!")
+    (System/exit 1))
+
+  (println "Starting the borbot")
+  (<!! (p/start token handler)))
